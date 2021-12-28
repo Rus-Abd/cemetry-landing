07e492fe0f66d52a9adcace095f3a16378d19a6b
ruslan <ruslan.abdallah2000@gmail.com>
2021-12-29 01:06:52 +0300

    fix: improve site speed

--- app.js
+++ app.js
@@ -1,47 +1,50 @@
 const prevArrow = document.querySelector('.arrow-previous img');
 const nextArrow = document.querySelector('.arrow-next img');
-var slider = tns({
-  container: '.my-slider',
+window.onload = () => {
+  var slider = tns({
+    container: '.my-slider',
 
-  swipeAngle: false,
-  speed: 400,
-  controls: false,
-  nav: false,
+    swipeAngle: false,
+    speed: 400,
+    controls: false,
+    nav: false,
 
-  responsive: {
-    1170: { fixedWidth: 251, gutter: 76, mouseDrag: false, items: 3 },
-    678: { fixedWidth: 220, gutter: 36, mouseDrag: true, items: 3 },
-    320: { fixedWidth: 180, gutter: 25, mouseDrag: true, items: 1 },
-  },
-});
-slider.getInfo();
-var info = slider.getInfo(),
-  indexPrev = info.indexCached,
-  indexCurrent = info.index;
-let prevSlide = 0;
-let currentSlide = 0;
-nextArrow.addEventListener('click', () => {
-  if (currentSlide == 4) {
-    slider.goTo(0);
-    prevSlide = currentSlide;
-    currentSlide = 0;
-  } else {
-    prevSlide = currentSlide;
-    currentSlide += 1;
-    slider.goTo(currentSlide);
-  }
-});
-prevArrow.addEventListener('click', () => {
-  if (currentSlide == 0) {
-    slider.goTo(4);
-    prevSlide = currentSlide;
-    currentSlide = 4;
-  } else {
-    prevSlide = currentSlide;
-    currentSlide -= 1;
-    slider.goTo(currentSlide);
-  }
-});
+    responsive: {
+      1170: { fixedWidth: 251, gutter: 76, mouseDrag: false, items: 3 },
+      678: { fixedWidth: 220, gutter: 36, mouseDrag: true, items: 3 },
+      320: { fixedWidth: 180, gutter: 25, mouseDrag: true, items: 1 },
+    },
+  });
+  slider.getInfo();
+  var info = slider.getInfo(),
+    indexPrev = info.indexCached,
+    indexCurrent = info.index;
+  let prevSlide = 0;
+  let currentSlide = 0;
+  nextArrow.addEventListener('click', () => {
+    if (currentSlide == 4) {
+      slider.goTo(0);
+      prevSlide = currentSlide;
+      currentSlide = 0;
+    } else {
+      prevSlide = currentSlide;
+      currentSlide += 1;
+      slider.goTo(currentSlide);
+    }
+  });
+  prevArrow.addEventListener('click', () => {
+    if (currentSlide == 0) {
+      slider.goTo(4);
+      prevSlide = currentSlide;
+      currentSlide = 4;
+    } else {
+      prevSlide = currentSlide;
+      currentSlide -= 1;
+      slider.goTo(currentSlide);
+    }
+  });
+};
+
 //
 //
 // 52.126424070008916, 26.110533197336057
@@ -71,11 +74,12 @@ formButton.addEventListener('click', () => {
 const hamburger = document.querySelector('.hamburger');
 const dropMenu = document.querySelector('.drop-down');
 hamburger.addEventListener('click', () => {
-  hamburger.style.opacity = 0.4;
   if (dropMenu.style.display === 'none' || !dropMenu.style.display) {
     dropMenu.style.display = 'flex';
+    hamburger.style.opacity = 0.4;
   } else {
     dropMenu.style.display = 'none';
+    hamburger.style.opacity = 1;
   }
 });
 window.addEventListener('resize', () => {
